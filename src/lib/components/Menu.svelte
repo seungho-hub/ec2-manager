848be2e511f292629b92d968db2c8717f0cc45a9
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-12-08 01:39:58 +0900

    add menu itme : refresh instances

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,7 +1,13 @@
 <script>
+	let { reloadInstances } = $props();
 </script>
 
 <section id="menu">
+	<div class="item">
+		<button class="reload" on:click={() => reloadInstances()}>
+			<span class="material-symbols-outlined"> refresh </span>
+		</button>
+	</div>
 	<div class="item">
 		<button class="create-instance"> create instance </button>
 	</div>
@@ -9,11 +15,25 @@
 
 <style lang="scss">
 	#menu {
+		display: flex;
+		flex-direction: row;
+		gap: 10px;
 		.item {
 			.create-instance {
 				background-color: var(--sig);
 				color: var(--font-black);
 				font-weight: bold;
+				height: 30px;
+			}
+			.reload {
+				width: 30px;
+				height: 30px;
+				background-color: var(--font-light-gray);
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				border-radius: 15px;
+				font-size: 10px;
 			}
 		}
 	}
