94acaff56fa34c306dfa8a86ebc0dab4ce048585
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2024-12-08 01:01:15 +0900

    add activation menu

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<section id="menu">
+	<div class="item">
+		<button class="create-instance"> create instance </button>
+	</div>
+</section>
+
+<style lang="scss">
+	#menu {
+		.item {
+			.create-instance {
+				background-color: var(--sig);
+				color: var(--font-black);
+				font-weight: bold;
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
+	import Menu from '$lib/components/Menu.svelte';
 </script>
 
-<section id="main"></section>
+<section id="main">
+	<Menu />
+</section>
 
 <style lang="scss">
 	#main {
-		margin-top: 20px;
+		margin: 20px 40px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
 	}
 </style>
